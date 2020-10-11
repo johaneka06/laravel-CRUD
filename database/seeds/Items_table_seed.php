738383e97e1dbd7fcf77b8a627dbccc536e338a2
Johan Santosa <johanekasantosa@yahoo.com>
2020-10-11 23:22:50 +0700

    Fix seeder problem

--- database/seeds/Items_table_seed.php
+++ database/seeds/Items_table_seed.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Item;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -12,22 +13,22 @@ class Items_table_seed extends Seeder
      */
     public function run()
     {
-        DB::table("items")->insert(
-            [
-                "name" => "Zasus 4GB RAM",
-                "price" => 450000,
-                "stock" => 15
-            ],
-            [
-                "name" => "RAG PC Motherboard",
-                "price" => 1500000,
-                "stock" => 20
-            ],
-            [
-                "name" => "Zumsang LED Monitor",
-                "price" => 2500000,
-                "stock" => 5
-            ]
-        );
+        Item::create([
+            "name" => "Zasus 4GB RAM",
+            "price" => 450000,
+            "stock" => 15
+        ]);
+
+        Item::create([
+            "name" => "RAG PC Motherboard",
+            "price" => 1500000,
+            "stock" => 20
+        ]);
+
+        Item::create([
+            "name" => "Zumsang LED Monitor",
+            "price" => 2500000,
+            "stock" => 5
+        ]);
     }
 }
